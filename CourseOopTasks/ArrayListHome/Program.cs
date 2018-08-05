210538094cb475a86d346e9b2979a8a769a8be5a
sssclass <sssclass@hotmail.com>
2018-08-06 00:46:29 +0600

    ArrayListHome на проверку

--- CourseOopTasks/ArrayListHome/Program.cs
+++ CourseOopTasks/ArrayListHome/Program.cs
@@ -11,11 +11,45 @@ namespace ArrayListHome
     {
         static void Main(string[] args)
         {
-            using (StreamReader reader = new StreamReader("..\\..\\IntList.txt", Encoding.GetEncoding(1251))
+            using (StreamReader reader = new StreamReader("..\\..\\inputTxt.txt"))
             {
+                List<int> numbers = new List<int>();
                 string line;
-            while ((line = reader.ReadLine()) != null)
-            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] numbersStrings = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);                    
+                    for (int i = 0; i < numbersStrings.Length; i++)
+                    {
+                        numbers.Add(Convert.ToInt32(numbersStrings[i]));                       
+                    }                                       
+                }
+                Console.WriteLine("исходный список: ");
+                Console.WriteLine(string.Join(",", numbers));
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        numbers.RemoveAt(i);
+                        --i;                        
+                    }
+                }
+                Console.WriteLine("список без чётных чисел: ");
+                Console.WriteLine(string.Join(",", numbers));
+
+                List<int> numbers2 = new List<int>();
+                numbers2.Add(numbers[0]);
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    bool isDeleted = numbers.Remove(numbers2[i]);                    
+                    while (isDeleted)
+                    {
+                        isDeleted = numbers.Remove(numbers2[i]);
+                    }
+                    numbers2.Add(numbers[0]);
+                }
+                Console.WriteLine("список без повторения чисел: ");
+                Console.WriteLine(string.Join(",", numbers2));
             }
+        }  
     }
 }
