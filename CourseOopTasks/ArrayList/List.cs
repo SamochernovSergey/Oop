97dad2e4332978ecfe64aa45ddf20a2c0591041f
Сергей Серж <sss@kzz-club.ru>
2019-04-01 23:33:02 +0700

    Реализация списка на массиве.

---
+++ CourseOopTasks/ArrayList/List.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayList
+{
+    class List<T> : IList<T>
+    {
+        private T[] array = new T[10];
+
+        private int length;
+
+        public List()
+        {             
+        }
+
+        public List(T[] array)
+        {            
+            this.array = new T[array.Length + 5];
+            Array.Copy(array, this.array, array.Length);
+            length = array.Length;            
+        }
+
+        public List(int capacity)
+        {
+            array = new T[capacity];
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return array.Length;
+            }
+            set
+            {
+                if (value < length)
+                {
+                    throw new ArgumentOutOfRangeException("Capacity < Count");
+                }
+
+                T[] array = this.array;
+                this.array = new T[value];
+                Array.Copy(array, this.array, length);                                
+            }
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] old = array;
+            array = new T[old.Length * 2];
+            Array.Copy(old, 0, array, 0, old.Length);
+        }
+
+        private void EnsureCapacity()
+        {
+            if (length >= array.Length)
+            {
+                IncreaseCapacity();
+            }
+        }
+
+        public void TrimExcess()
+
+        {
+            if (Capacity * 0.1 > length)
+            {
+                Array.Resize(ref array, length);
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= length)
+                {
+                    throw new IndexOutOfRangeException("Argument Out Of Range");
+                }
+                return array[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= length)
+                {
+                    throw new IndexOutOfRangeException("Argument Out Of Range");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T obj)
+        {
+            EnsureCapacity();
+            array[length] = obj;
+            ++length;
+        }
+
+        public void Clear()
+        {            
+            array = new T[10];
+            length = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            if (IndexOf(item) != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new IndexOutOfRangeException("Argument Out Of Range");
+            }
+
+            if (length > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("The size of the data is larger than the array Count");
+            }
+
+            int j = arrayIndex;
+            foreach (T e in this.array)
+            {
+                array[j] = e;
+                j++;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < length; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (Equals(array[i], item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > length)
+            {
+                throw new IndexOutOfRangeException("Argument Out Of Range");
+            }
+
+            EnsureCapacity();
+            ++length;
+
+            for (int i = length - 1; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            
+            array[index] = item;            
+        }
+
+        public bool Remove(T item)
+        {
+            if (IndexOf(item) != -1)
+            {
+                RemoveAt(IndexOf(item));
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > length - 1)
+            {
+                throw new IndexOutOfRangeException("Argument Out Of Range");
+            }
+
+            Array.Copy(array, index + 1, array, index, length - index - 1);
+            --length;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < Count; i++)
+            {
+                stringBuilder.Append(array[i]).Append(";").Append(Environment.NewLine);                
+            }
+
+            stringBuilder.Append("Count = ").Append(Count).Append(Environment.NewLine);
+            stringBuilder.Append("Capacity = ").Append(Capacity);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ CourseOopTasks/ArrayList/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
+            Console.WriteLine(strings);
+
+            List<int> list = new List<int>(9);
+            Console.WriteLine(list);
+
+            List<int> myList = new List<int>
+            {
+                14,
+                45,
+                78,
+                57,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14
+            };
+            myList.Insert(9, 99);
+
+            Console.WriteLine(myList.Contains(99));
+            Console.WriteLine(myList.Contains(100));
+            Console.WriteLine(myList);
+
+            int x = myList[3];
+            Console.WriteLine(x);
+
+            myList[3] = 188;
+            Console.WriteLine(myList);
+            Console.WriteLine("Элемент в списке 78, находится по индексу = {0}", myList.IndexOf(78));
+
+            myList.Add(33);
+            myList.Add(34);
+            Console.WriteLine(myList);
+
+            Console.WriteLine(myList.Remove(99));
+
+            myList.RemoveAt(9);
+            Console.WriteLine(myList);
+
+            Console.WriteLine("Проверка итератора");
+
+            foreach (int e in myList)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("конец проверки итератора");
+            Console.WriteLine("проверка TrimExcess");
+            myList.Capacity = 120;
+            Console.WriteLine("до TrimExcess");
+            Console.WriteLine(myList);
+
+            myList.TrimExcess();
+            Console.WriteLine("после TrimExcess");
+            Console.WriteLine(myList);
+
+            int[] p = { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> list2 = new List<int>(p);
+            Console.WriteLine(list2);
+
+            int[] myList2 = new int[20];
+            list2.CopyTo(myList2,2);
+
+            foreach (int e in myList2)
+            {
+                Console.WriteLine(e);
+            }
+
+            list2.Clear();
+            Console.WriteLine(list2);
+        }
+    }
+}
