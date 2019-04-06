9acfa6fd9e144788f6ec7874735a1ac1f2342711
Сергей Серж <sss@kzz-club.ru>
2019-04-06 23:19:08 +0700

    Реализация Hash Table

---
+++ CourseOopTasks/HashTable/HashTable.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTable<T> : ICollection<T>
+    {
+        private List<T>[] table;
+        private int version;
+
+        public HashTable()
+        {
+            table = new List<T>[50];
+        }
+
+        public HashTable(int capacity)
+        {
+            table = new List<T>[capacity];
+        }
+
+        private int GetHashIndex(T item)
+        {
+            return Math.Abs(item.GetHashCode() % table.Length);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return table.Length;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            int index = GetHashIndex(item);
+
+            if (table[index] == null)
+            {
+                table[index] = new List<T>();
+            }
+
+            if (table[index].Contains(item))
+            {
+                return;
+            }
+
+            table[index].Add(item);
+            ++version;
+        }
+
+        public void Clear()
+        {
+            if (table.Any())
+            {
+                table = new List<T>[50];
+                ++version;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            int index = GetHashIndex(item);
+
+            if (table[index] == null)
+            {
+                return false;
+            }
+
+            return table[index].Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new IndexOutOfRangeException("Argument Out Of Range");
+            }
+
+            for (int i = 0; i < table.Length; i++)
+            {                
+                if (table[i] != null)
+                {
+                    for (int j = 0; j < table[i].Count; j++)
+                    {                        
+                        array[arrayIndex] = table[i].ElementAt(j);
+                        arrayIndex++;
+                    }
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int trueVersion = version;
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                if (table[i] != null)
+                {
+                    for (int j = 0; j < table[i].Count; j++)
+                    {
+                        if (trueVersion != version)
+                        {
+                            throw new InvalidOperationException("don't change collection when Enumirator is working!!!");
+                        }
+
+                        yield return table[i].ElementAt(j);
+                    }
+                }
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int index = GetHashIndex(item);
+
+            if (table[index].Contains(item))
+            {
+                table[index] = null;
+                ++version;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                stringBuilder.Append("array[").Append(String.Format("{0:d2}", i)).Append("]").Append(" = ");
+
+                if (table[i] == null)
+                {
+                    stringBuilder.Append("null").AppendLine("; ");
+                }
+                else
+                {
+                    for (int j = 0; j < table[i].Count; j++)
+                    {
+                        stringBuilder.Append((table[i].ElementAt(j))).Append(", ");
+                    }
+
+                    stringBuilder.Remove(stringBuilder.Length - 2, 2);
+                    stringBuilder.AppendLine("; ");
+                }
+            }
+            stringBuilder.Append("Count = ").Append(Count).AppendLine();
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ CourseOopTasks/HashTable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> table = new HashTable<int>(15)
+            {
+                11,
+                22,
+                33,
+                88,
+                44,
+                55,
+                55
+            };
+
+            table.Remove(88);
+            Console.WriteLine(table);
+
+            foreach (var e in table)
+            {
+                Console.WriteLine(e);                
+            }
+
+            Console.WriteLine();
+
+            int[] p = new int[20];
+
+            table.CopyTo(p, 9);
+
+            Console.WriteLine("CopyTo");
+
+            foreach (int e in p)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
